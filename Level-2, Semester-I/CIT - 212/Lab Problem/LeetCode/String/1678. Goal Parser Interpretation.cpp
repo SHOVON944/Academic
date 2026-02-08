a94525fd07488346c897f8c83c40b2ef130d753b
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2026-02-08 08:45:57 +0600

    add

---
+++ Level-2, Semester-I/CIT - 212/Lab Problem/LeetCode/String/1678. Goal Parser Interpretation.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    string interpret(string command) {
+        string result;
+        for(int i=0; i<command.size(); i++){
+            if(command[i]=='G'){
+                result += 'G';
+            } else if(command[i]=='('   &&  command[i+1]==')'){
+                result += 'o';
+                i++;
+            } else{
+                result += "al";
+                i += 3;
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+
+
+    return 0;
+}
