8cf4c1ca2cb1c1f1065ca9a2c9ede200799b2b82
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2026-02-24 07:19:46 +0600

    add

--- Level-2, Semester-I/CIT - 212/Lab Problem/Chapter/Chapter - 04/Algo 4.1 (traversing while loop).cpp
+++ Level-2, Semester-I/CIT - 212/Lab Problem/Chapter/Chapter - 04/Algo 4.1 (traversing while loop).cpp
@@ -1,3 +1,10 @@
+/*
+Algorithm 4.1: (Traversing a Linear Array) This algorithm traverses a linear array LA with lower bound LB and upper bound UB.
+1.	Repeat for K = LB to UB: Apply PROCESS to LA[K]. [End of loop.]
+2.	Exit.
+
+*/
+
 #include <iostream>
 using namespace std;
 
