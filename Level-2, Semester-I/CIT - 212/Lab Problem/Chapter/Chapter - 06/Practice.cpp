2a72dd521e89d9c1d474d132832d40b134551317
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2026-03-02 17:19:04 +0600

    add

--- Level-2, Semester-I/CIT - 212/Lab Problem/Chapter/Chapter - 06/Practice.cpp
+++ Level-2, Semester-I/CIT - 212/Lab Problem/Chapter/Chapter - 06/Practice.cpp
@@ -12,7 +12,7 @@ void Qinsert(int queue[],int n,int &front, int  &rear,int item){
         front = 0;
         rear = 0;
     } else if(rear==n-1){
-        rear==0;
+        rear=0;
     } else{
         rear = rear + 1;
     }
@@ -39,6 +39,20 @@ void display(int queue[], int n,int front, int  rear){
     cout<<endl;
 }
 
+void QDELETE(int queue[], int n, int &front, int &rear, int &item){
+    if(front==NULLPTR) return;
+
+    item = queue[front];
+    if(front == rear){
+        front = NULLPTR;
+        rear = NULLPTR;
+    } else if(front==n-1){
+        front = 0;
+    } else{
+        front = front + 1;
+    }
+}
+
 int main()
 {
     int n = 5;
@@ -49,7 +63,11 @@ int main()
     Qinsert(queue, n, front, rear, item);
     Qinsert(queue, n, front, rear, 30);
     Qinsert(queue, n, front, rear, 40);
+    int ITEM;
+
     display(queue, n, front, rear);
+    QDELETE(queue, n, front, rear, ITEM);
+    cout<<ITEM<<endl;
 
     return 0;
 }
