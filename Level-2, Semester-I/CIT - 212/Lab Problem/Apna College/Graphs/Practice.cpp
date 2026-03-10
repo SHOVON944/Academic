9351c1cd0364dd726063eafdf90c3a934787f8fb
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2026-03-10 17:00:51 +0600

    add

--- Level-2, Semester-I/CIT - 212/Lab Problem/Apna College/Graphs/Practice.cpp
+++ Level-2, Semester-I/CIT - 212/Lab Problem/Apna College/Graphs/Practice.cpp
@@ -1,49 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-public:
-    int data;
-    Node* next;
+int main() {
+    int M;
 
-    Node(int val){
-        data = val;
-        next = NULL;
-    }
-};
+    cout << "Enter number of vertices: ";
+    cin >> M;
 
-class List{
-    Node* head;
-    Node* tail;
+    int A[M][M];
+    int P[M][M];
 
-public:
-    List(){
-        head = tail = NULL;
-    }
-
-    void push_back(int val){
-        Node* newNode = new Node(val);
-
-        if(head==NULL){
-            head = tail = newNode;
-        }
-
-        tail->next = newNode;
-        tail = newNode;
-    }
-
-    void print(){
-        Node* temp = head;
-        while(temp != NULL){
-            cout<<temp->data<<" ";
-            temp = temp->next;
+    cout << "Enter Adjacency Matrix:\n";
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < M; j++){
+            cin >> A[i][j];
         }
     }
-};
 
-int main()
-{
+    // Step 1: Initialize P from A
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < M; j++){
+            if(A[i][j] == 0)
+                P[i][j] = 0;
+            else
+                P[i][j] = 1;
+        }
+    }
 
+    // Warshall's Algorithm
+    for(int k = 0; k < M; k++){
+        for(int i = 0; i < M; i++){
+            for(int j = 0; j < M; j++){
+                P[i][j] = P[i][j] || (P[i][k] && P[k][j]);
+            }
+        }
+    }
+
+    cout << "\nPath Matrix (Transitive Closure):\n";
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < M; j++){
+            cout << P[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
