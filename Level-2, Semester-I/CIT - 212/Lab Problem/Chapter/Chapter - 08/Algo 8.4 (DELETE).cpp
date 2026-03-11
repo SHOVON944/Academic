4698273def09faa8d85f2b8a3276d464c8ab94ac
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2026-03-11 06:39:19 +0600

    add

--- Level-2, Semester-I/CIT - 212/Lab Problem/Chapter/Chapter - 08/Algo 8.4 (DELETE).cpp
+++ Level-2, Semester-I/CIT - 212/Lab Problem/Chapter/Chapter - 08/Algo 8.4 (DELETE).cpp
@@ -1,43 +1,85 @@
+/*
+Like as book
+void DELETE(int item){
+    if(head == NULL){
+        cout<<"List is empty. Cannot delete.\n";
+        return;
+    }
+
+    // First node check
+    if(head->data == item){
+        Node* PTR = head;
+        head = head->next;
+        if(PTR == tail) tail = NULL; // single node case
+        delete PTR;
+        cout << "Item deleted from the first node.\n";
+        return;
+    }
+
+    // Middle / Last node
+    Node* prev = head;
+    Node* curr = head->next;
+
+    while(curr != NULL){
+        if(curr->data == item){
+            prev->next = curr->next;
+            if(curr == tail) tail = prev; // last node delete
+            delete curr;
+            cout << "Item deleted from the list.\n";
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    cout << "Item not found. Cannot delete.\n";
+}
+*/
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node {
+class Node{
 public:
     int data;
     Node* next;
 
-    Node(int val) {
+    Node(int val){
         data = val;
         next = NULL;
     }
 };
 
-class List {
+class List{
     Node* head;
     Node* tail;
 
 public:
-    List() {
+    List(){
         head = tail = NULL;
     }
 
-    void push_back(int val) {
+    void push_back(int val){
         Node* newNode = new Node(val);
-        if (head == NULL) {
+
+        if(head == NULL){
             head = tail = newNode;
-        } else {
+        }
+        else{
             tail->next = newNode;
             tail = newNode;
         }
     }
 
-    void printLL() {
+    void printLL(){
         Node* temp = head;
-        while (temp != NULL) {
-            cout << temp->data << " -> ";
+        while(temp != NULL){
+            cout<<temp->data << " -> ";
             temp = temp->next;
         }
-        cout << "NULL" << endl;
+        cout<<"NULL"<<endl;
     }
 
     void DELETE(int item){
@@ -47,16 +89,26 @@ public:
         }
 
         Node* PTR = head;
+        Node* PREV = NULL;  // previous node, initially NULL
+
         while(PTR != NULL){
             if(PTR->data == item){
-                head = head->next;
+                if(PTR == head){                    // first node
+                    head = PTR->next;
+                    if(PTR == tail) tail = NULL;    // single node case
+                } else{                            // middle or last node
+                    PREV->next = PTR->next;
+                    if(PTR == tail) tail = PREV;    // last node update
+                }
+
                 delete PTR;
-                cout << "Item deleted from the first node.\n";
+                cout << "Item deleted successfully."<<endl;
                 return;
-            } else{
-                PTR = PTR->next;
             }
+            PREV = PTR;
+            PTR = PTR->next;
         }
+        cout << "Item not found. Cannot delete.";
     }
 };
 
@@ -64,26 +116,21 @@ int main()
 {
     List ll;
     int n, val;
-
-    cout << "Enter number of nodes: ";
-    cin >> n;
-
-    cout << "Enter values:" << endl;
-    for (int i = 0; i < n; i++) {
-        cin >> val;
+    cout<<"Enter number of nodes: ";
+    cin>>n;
+    cout<<"Enter values: ";
+    for(int i=0; i<n; i++){
+        cin>>val;
         ll.push_back(val);
     }
-
-    cout << "Linked List: ";
+    cout<<"Linked List: ";
     ll.printLL();
 
     int item;
-    cout << "Enter item to delete: ";
-    cin >> item;
-
+    cout<<"Enter item to delete: ";
+    cin>>item;
     ll.DELETE(item);
-
-    cout << "Linked List after deletion: ";
+    cout<<"Linked List after deletion: ";
     ll.printLL();
 
     return 0;
