ba43bbd3f1643e9c85a42dfb4c8dc00e9fddec69
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2026-03-18 21:50:06 +0600

    add

---
+++ Level-2, Semester-I/CIT - 212/Lab Problem/Chapter/Chapter - 08/Simple/Algo 8.5 (find Edge).cpp
@@ -0,0 +1,161 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+//   LL
+class List{
+    Node* head;
+    Node* tail;
+
+public:
+    List(){
+        head = tail = NULL;
+    }
+
+    void push_back(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+            return;
+        }
+
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    Node* find(int ITEM){
+        Node* temp = head;
+
+        while(temp != NULL){
+            if(temp->data == ITEM){
+                return temp;
+            }
+            temp = temp->next;
+        }
+
+        return NULL;
+    }
+
+    void print(){
+        Node* temp = head;
+        while(temp != NULL){
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+    }
+};
+
+// ---------- Graph
+class Graph{
+    int V;
+    List* adj;
+
+public:
+    Graph(int V){
+        this->V = V;
+        adj = new List[V];
+    }
+
+    void addEdge(int u,int v){
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    void findEdgePrint(int A,int B){
+        Node* LOC = adj[A].find(B);   // 🔥
+
+        if(LOC != NULL){
+            cout<<"Edge ("<<A<<","<<B<<") found at address: "<<LOC<<endl;
+        } else{
+            cout<<"Edge ("<<A<<","<<B<<") not found (LOC = NULL)"<<endl;
+        }
+    }
+
+    void printAdjList(){
+        for(int i=0;i<V;i++){
+            cout<<i<<": ";
+            adj[i].print();
+            cout<<endl;
+        }
+    }
+};
+
+int main()
+{
+    Graph g(5);
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(1,3);
+    g.addEdge(2,3);
+    g.addEdge(2,4);
+    cout<<"Adjacency List:"<<endl;
+    g.printAdjList();
+    cout<<endl;
+
+    g.findEdgePrint(1,3);   // exist
+    g.findEdgePrint(0,4);   // not exist
+
+    return 0;
+}
+
+
+/*
+    * Input
+        Enter number of vertices: 5
+        Enter number of edges: 5
+        Enter edges (u v):
+        0 1
+        1 2
+        1 3
+        2 3
+        2 4
+
+    * Output
+        Adjacency List:
+        0: 1
+        1: 0 2 3
+        2: 1 3 4
+        3: 1 2
+        4: 2
+
+        Enter edge to search (A B): 1 3
+        Edge (1,3) found at address: 0x55a...
+*/
+
+/*
+int main()
+{
+    int V, E;
+    cout<<"Enter number of vertices: ";
+    cin>>V;
+    Graph g(V);
+    cout<<"Enter number of edges: ";
+    cin>>E;
+    cout<<"Enter edges (u v):"<<endl;
+    for(int i=0;i<E;i++){
+        int u,v;
+        cin>>u>>v;
+        g.addEdge(u,v);
+    }
+    cout<<"Adjacency List:"<<endl;
+    g.printAdjList();
+
+    int A,B;
+    cout<<"Enter edge to search (A B): ";
+    cin>>A>>B;
+
+    g.findEdgePrint(A,B);
+
+    return 0;
+}
+*/
