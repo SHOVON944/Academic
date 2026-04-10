8de6aa9186b8d02f1fd695633f4b933e3e43599d
MD. SAKIBUL ISLAM SHOVON <mdsakibulislamshovon944@gmail.com>
2026-04-10 11:41:00 +0600

    add

--- Level-2, Semester-I/CIT - 212/Lab Problem/Algorithm/Bellman Ford.cpp
+++ Level-2, Semester-I/CIT - 212/Lab Problem/Algorithm/Bellman Ford.cpp
@@ -1,26 +1,170 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-void selectionSort(vector<int>& arr){
-    int n = arr.size();
-    for(int i = 0 ; i < n-1 ; i++) {
-    bool flag = false;
-    for(int j = 0 ; j < n-i-1; j++) {
-        if(arr[j] > arr[j+1]) {
-            flag=true;
-            swap(arr[j],arr[j+1]);
+
+class Node{
+public:
+    int vertex;
+    int weight;
+    Node* next;
+
+    Node(int v,int w){
+        vertex = v;
+        weight = w;
+        next = NULL;
+    }
+};
+
+
+class List{
+    Node* head;
+
+public:
+    List(){
+        head = NULL;
+    }
+
+    void push_back(int v,int w){
+        Node* newNode = new Node(v,w);
+
+        if(head == NULL){
+            head = newNode;
+            return;
+        }
+
+        Node* temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+    }
+
+    Node* getHead(){
+        return head;
+    }
+};
+
+// -------- Graph
+class Graph{
+    int V;
+    List* adj;
+
+public:
+    Graph(int v){
+        V = v;
+        adj = new List[V];
+    }
+
+    void addEdge(int u,int v,int w){
+        adj[u].push_back(v,w);   // directed graph
+    }
+
+    // -------- Bellman-Ford
+    void BellManFord(int src){
+        int* dist = new int[V];
+
+        for(int i=0;i<V;i++){
+            dist[i] = 999999;
+        }
+
+        dist[src] = 0;
+
+        // V-1 times relax
+        for(int i=0;i<V-1;i++){
+            for(int u=0;u<V;u++){
+                Node* temp = adj[u].getHead();
+
+                while(temp != NULL){
+                    int v = temp->vertex;
+                    int w = temp->weight;
+
+                    if(dist[u] != 999999 && dist[v] > dist[u] + w){
+                        dist[v] = dist[u] + w;
+                    }
+
+                    temp = temp->next;
+                }
+            }
+        }
+
+        // ---- Print result
+        cout<<"Shortest distances from source "<<src<<":"<<endl;
+        for(int i=0;i<V;i++){
+            cout<<"Vertex "<<i<<" = "<<dist[i]<<endl;
         }
     }
-    if(!flag) break;
-    }
+};
+
+//! TC -> O(V.E)
+int main()
+{
+    Graph g(5);
+
+    g.addEdge(0,1,2);
+    g.addEdge(0,2,4);
+
+    g.addEdge(1,4,-1);
+    g.addEdge(1,2,-4);
+
+    g.addEdge(2,3,2);
+
+    g.addEdge(3,4,4);
+
+    g.BellManFord(0);
+
+    return 0;
 }
+
+
+
+
+/*
+*Input:
+    Enter number of vertices: 5
+    Enter number of edges: 6
+    Enter edges (u v w):
+    0 1 2
+    0 2 4
+    1 4 -1
+    1 2 -4
+    2 3 2
+    3 4 4
+    Enter source vertex: 0
+
+*Output:
+    0 2 -2 0 1
+
+
 
 int main()
 {
-    vector<int> arr = {5, 2, 4, 6, 1, 3};
-    selectionSort(arr);
-    cout << "Selection Sort: ";
-    for (int num : arr)
-        cout << num << " ";
+    int V,E;
+    cout<<"Enter number of vertices: ";
+    cin>>V;
+
+    Graph g(V);
+
+    cout<<"Enter number of edges: ";
+    cin>>E;
+
+    cout<<"Enter edges (u v w):"<<endl;
+    for(int i=0;i<E;i++){
+        int u,v,w;
+        cin>>u>>v>>w;
+        g.addEdge(u,v,w);
+    }
+
+    int src;
+    cout<<"Enter source vertex: ";
+    cin>>src;
+
+    cout<<"\nAdjacency List:"<<endl;
+    g.printAdjList();
+
+    g.bellmanFord(src);
+
     return 0;
 }
+
+*/
